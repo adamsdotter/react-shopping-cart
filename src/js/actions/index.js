65725d003df1db27132287ca0f4867ec595e0ee8
Patricia Adamsson <patricia.adamsson@gmail.com>
2019-09-18 23:19:24 +0200

    add decrement

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,8 @@
-import { FETCH_PRODUCTS, ADD_TO_CART, INCREMENT_ITEM } from '../constants/action-types';
+import { FETCH_PRODUCTS, ADD_TO_CART, INCREMENT_ITEM, DECREMENT_ITEM } from '../constants/action-types';
 
 export const fetchProducts = () => ({ type: FETCH_PRODUCTS });
 
 export const addToCart = payload => ({ type: ADD_TO_CART, payload });
 
 export const incrementItem = payload => ({ type: INCREMENT_ITEM, payload });
+export const decrementItem = payload => ({ type: DECREMENT_ITEM, payload });
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -4,8 +4,9 @@ import rootReducer from '../reducers/';
 import fetchProducts from '../middleware/fetchProducts';
 import addToCart from '../middleware/addToCart';
 import incrementItem from '../middleware/incrementItem';
+import decrementItem from '../middleware/decrementItem';
 
-const middleWares = [fetchProducts, addToCart, incrementItem];
+const middleWares = [fetchProducts, addToCart, incrementItem, decrementItem];
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
