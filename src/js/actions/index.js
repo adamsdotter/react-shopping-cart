75600704f64321b57879ca9d67fa84a2d94a5d81
Patricia Adamsson <patricia.adamsson@gmail.com>
2019-09-18 17:55:09 +0200

    add expandable cart and cart-item

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -3,5 +3,4 @@ import { FETCH_PRODUCTS, ADD_TO_CART, FETCH_CART_COUNT } from '../constants/acti
 export const fetchProducts = () => ({ type: FETCH_PRODUCTS });
 
 export const fetchCartCount = () => ({ type: FETCH_CART_COUNT });
-
 export const addToCart = payload => ({ type: ADD_TO_CART, payload });
---
+++ src/js/components/CartOverview/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CartItem from '../CartItem/';
+
+const getItem = (product) => {
+  const item = product.items[0].product;
+
+  return <CartItem key={item.id} item={item} />;
+};
+
+const CartOverview = ({ items }) => {
+  return (
+    <div className="cart-overview">
+      <h1>ALL PRODUCTS</h1>
+      { items ? items.map(item => getItem(item)) : null }
+    </div>
+  );
+}
+
+CartOverview.propTypes = {
+  items: PropTypes.array.isRequired
+}
+
+export default CartOverview;
