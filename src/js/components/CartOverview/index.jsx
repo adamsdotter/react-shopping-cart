30bb0b5e6e268a94a24fd71e48dd1e7c366843bd
Patricia Adamsson <patricia.adamsson@zmartagroup.com>
2019-09-19 14:29:28 +0200

    style for empty cart

--- src/js/components/CartOverview/index.jsx
+++ src/js/components/CartOverview/index.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import CartItem from '../CartItem/';
 import './cart-overview.scss';
 
-const getItem = (item) => {
-  return <CartItem key={item.id} item={item} />
-};
+const getItem = item => <li><CartItem key={item.id} item={item} /></li>;
 
 const CartOverview = ({ items, sumTotal }) => {
   return (
     <div className="cart-overview">
-      { items ? items.map(item => getItem(item)) : null }
-      <p>Total sum { sumTotal }</p>
+      { items.length
+        ? (
+          <div>
+            <ul>{items.map(item => getItem(item))}</ul>
+            <p>Total sum { sumTotal }</p>
+          </div>
+        )
+        : <p className="empty-cart">Your cart is empty <span aria-hidden>&#128546;</span></p>
+      }
     </div>
   );
 }
