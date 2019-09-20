ac669c285a08e79c2a750c6006e11e506e9f1791
Patricia Adamsson <patricia.adamsson@gmail.com>
2019-09-20 11:50:30 +0200

    refactor product

--- src/js/components/Products/index.jsx
+++ src/js/components/Products/index.jsx
@@ -1,45 +1,34 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { addToCart, fetchProducts } from '../../actions/';
 import Product from '../Product';
 import './products.scss';
 
-// refactor
-const mapDispatchToProps = dispatch => {
-  return {
-    addToCart: id => dispatch(addToCart(id)),
-    fetchProducts: () => dispatch(fetchProducts())
-  };
-};
+export default function Products() {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+  const products = state.products.items;
+  const cartItems = state.cart.items;
 
-const mapStateToProps = state => {
-  return {
-    items: state.products.items,
-    cartItems: state.cart.items
-  };
-};
-
-function Products({ items, cartItems, addToCart, fetchProducts }) {
   const getGroducts = () => {
-    return items.length
-      ? items.map((item) => (
+    return products.length
+      ? products.map((item) => (
         <li key={item.id}>
           <Product item={item} onClick={onClick.bind(this)} inCart={isItemInCart(item.id)} />
         </li>
       ))
-      : items;
+      : products;
   }
 
   const isItemInCart = (id) => cartItems.filter(el => el.id === id).length;
-  const onClick = (id) => addToCart(id);
+  const onClick = (id) => dispatch(addToCart(id));
 
   useEffect(
     () => {
-      if (!items.length) {
-        fetchProducts();
+      if (!products.length) {
+        dispatch(fetchProducts());
       }
-    }, [items]
+    }, [products]
   )
 
   return (
@@ -49,10 +38,3 @@ function Products({ items, cartItems, addToCart, fetchProducts }) {
     </div>
   );
 }
-
-Products.propTypes = {
-  items: PropTypes.array.isRequired,
-  cartItems: PropTypes.array.isRequired
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Products);
