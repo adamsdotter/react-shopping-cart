131e69a8da1fe1a38dafd10ed91c6295144f1057
Patricia Adamsson <patricia.adamsson@gmail.com>
2019-09-20 10:34:35 +0200

    refactor product

--- src/js/components/Products/index.jsx
+++ src/js/components/Products/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addToCart, fetchProducts } from '../../actions/';
 import Product from '../Product';
 import './products.scss';
 
+// refactor
 const mapDispatchToProps = dispatch => {
   return {
     addToCart: id => dispatch(addToCart(id)),
@@ -13,44 +15,44 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = state => {
   return {
-    products: state.products.items,
+    items: state.products.items,
     cartItems: state.cart.items
   };
 };
 
-class Products extends React.Component {
-  products() {
-    const { products } = this.props;
-
-    return products.length
-      ? products.map((item) => (
+function Products({ items, cartItems, addToCart, fetchProducts }) {
+  const getGroducts = () => {
+    return items.length
+      ? items.map((item) => (
         <li key={item.id}>
-          <Product item={item} onClick={this.onClick.bind(this)} inCart={this.isItemInCart(item.id)} />
+          <Product item={item} onClick={onClick.bind(this)} inCart={isItemInCart(item.id)} />
         </li>
       ))
-      : products;
+      : items;
   }
 
-  isItemInCart(id) {
-    return this.props.cartItems.filter(el => el.id === id).length;
-  }
+  const isItemInCart = (id) => cartItems.filter(el => el.id === id).length;
+  const onClick = (id) => addToCart(id);
 
-  onClick(id) {
-    this.props.addToCart(id);
-  }
+  useEffect(
+    () => {
+      if (!items.length) {
+        fetchProducts();
+      }
+    }, [items]
+  )
 
-  componentDidMount() {
-    this.props.fetchProducts();
-  }
+  return (
+    <div className="products">
+      <h2>Our products</h2>
+      <ul className="product-list">{getGroducts()}</ul>
+    </div>
+  );
+}
 
-  render() {
-    return (
-      <div className="products">
-        <h2>Our products</h2>
-        <ul className="product-list">{this.products()}</ul>
-      </div>
-    );
-  }
+Products.propTypes = {
+  items: PropTypes.array.isRequired,
+  cartItems: PropTypes.array.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
